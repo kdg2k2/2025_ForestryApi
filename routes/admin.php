60b2294e6a1d661a2d115bb16fbf06ff8afc81c5
Nguyen Van Anh <anhvanhoa.it@gmail.com>
2025-04-23 14:29:38 +0700

    Add a blank line for better readability in admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("dashboard", [DashboardController::class, 'index'])->name('dashboard');
+
 Route::prefix("users")->group(function () {
     Route::get("/", [UserController::class, 'index'])->name("admin.users.index");
     Route::get("/add", [UserController::class, 'add'])->name("admin.users.add");
