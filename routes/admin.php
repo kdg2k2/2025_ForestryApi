7ac2c67fd835cf8bd31f2283f0cc356880c0f0e7
Nguyen Van Anh <anhvanhoa.it@gmail.com>
2025-04-23 14:29:30 +0700

    Refactor admin routes: Move dashboard route outside of admin prefix

--- routes/admin.php
+++ routes/admin.php
@@ -4,10 +4,7 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix("admin")->group(function () {
-    Route::get("dashboard", [DashboardController::class, 'index'])->name('dashboard');
-});
-
+Route::get("dashboard", [DashboardController::class, 'index'])->name('dashboard');
 Route::prefix("users")->group(function () {
     Route::get("/", [UserController::class, 'index'])->name("admin.users.index");
     Route::get("/add", [UserController::class, 'add'])->name("admin.users.add");
